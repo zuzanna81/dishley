53d237617e0ed6242c80a0a212581095d3925b88
aljoh <reachingalfred@gmail.com>
2018-01-09 20:46:10 +0100

    modify coveralls for rails

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require 'coveralls'
-Coveralls.wear!
+Coveralls.wear_merged!('rails')
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
@@ -11,5 +11,4 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
-
 end
