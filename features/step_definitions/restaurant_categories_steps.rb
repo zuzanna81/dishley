8b8255982d1eb6c6f9282b7e6c7c05f136088d61
Zuzanna <zuzanna.bienkowska@gmail.com>
2018-01-12 15:07:04 +0100

    Modify step definitions for restaurant categories Modify index file

--- features/step_definitions/restaurant_categories_steps.rb
+++ features/step_definitions/restaurant_categories_steps.rb
@@ -5,7 +5,7 @@ Given("the following categories exist") do |table|
 end
 
 Then("I would like to see {string} in the {string} category") do |restaurant_name, category_name|
-  within "category##{category_name.titleize.gsub('', '').underscore}" do
+  within "#restaurant_category-#{category_name}" do
     expect(page).to have_content category_name
   end
 end
