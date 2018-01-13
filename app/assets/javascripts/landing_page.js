6f26324eeb77e7e257e03c1e151b59c38d27459b
aljoh <reachingalfred@gmail.com>
2018-01-13 20:52:42 +0100

    Markers for restaurants works, tests failing instead

--- app/assets/javascripts/landing_page.js
+++ app/assets/javascripts/landing_page.js
@@ -5,16 +5,5 @@ function initiateMap () {
         lat: 57.7089,
         lng: 11.9746
     });
-    geoLocate(57.7089,11.9746);
-}
-
-function geoLocate (lat, lng) {
-  map.addMarker({
-    lat: lat,
-    lng: lng,
-    title: "Somewhere",
-    click: function(e) {
-      alert('You clicked in this marker');
-    }
-  });
+    addMarkers();
 }
