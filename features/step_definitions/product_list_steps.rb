bd63188533f12cd80105f9a6ec0c6d3fe2924fe8
lisa_eriksson <lisa.c.eriksson@gmail.com>
2018-01-14 17:58:44 +0100

    WIP revise step defs

--- features/step_definitions/product_list_steps.rb
+++ features/step_definitions/product_list_steps.rb
@@ -1,6 +1,7 @@
-Given("the following products exist within a specific restaurant") do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
+Given("the following products exist within a specific restaurant") do |product|
+  table.hashes.each do |product|
+  FactoryBot.create(:product, product
+  )
 end
 
 When("I visit the {string} show page") do |string|
