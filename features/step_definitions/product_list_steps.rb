5c69f4a70d80546a15efa5631619a5290992443f
lisa_eriksson <lisa.c.eriksson@gmail.com>
2018-01-14 18:16:03 +0100

    Adds product model and factory

--- features/step_definitions/product_list_steps.rb
+++ features/step_definitions/product_list_steps.rb
@@ -1,9 +1,9 @@
-Given("the following products exist within a specific restaurant") do |product|
+Given("the following products exist within a specific restaurant") do |table|
   table.hashes.each do |product|
-  FactoryBot.create(:product, product
-  )
+    FactoryBot.create(:product, product)
+  end
 end
 
-When("I visit the {string} show page") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+When("I visit the {string} show page") do |restaurant_name|
+  visit "/#{restaurant_name}"
 end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'DB table' do
+    it { is_expected.to have_db_column :name }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :name }
+  end
+
+  describe 'Factory' do
+    it 'should be valid' do
+      expect(FactoryBot.create(:product)).to be_valid
+    end
+  end
+end
