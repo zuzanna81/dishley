9ad1a06f6afc0b45f27251d2d8618c76e8d934df
lisa_eriksson <lisa.c.eriksson@gmail.com>
2018-01-14 21:32:01 +0100

    Adds foreign key WIP

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
   validates :name, presence: true
 
-  belongs_to :restaurant
+  belongs_to :restaurant, foreign_key: true
 end
--- db/migrate/20180114201422_add_product_to_restaurants.rb
+++ db/migrate/20180114201422_add_product_to_restaurants.rb
@@ -1,5 +1,6 @@
 class AddProductToRestaurants < ActiveRecord::Migration[5.1]
   def change
-    add_column :restaurants, :products, :string
+    change_table :products do |t|
+      t.references :restaurants, foreign_key: true
   end
 end
