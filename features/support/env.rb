51d4e9eb769ef59e00440c5db7b7773722d1df59
aljoh <reachingalfred@gmail.com>
2018-01-15 10:47:33 +0100

    modified chromedriver

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,9 +4,9 @@ Chromedriver.set_version '2.33'
 
 Capybara.register_driver :chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: {
-          args: %w[ no-sandbox disable-popup-blocking window-size=1280,980]
-      }
+    chromeOptions: {
+      args: %w[headless no-sandbox disable-popup-blocking window-size=1280,980]
+    }
   )
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
@@ -19,7 +19,7 @@ ActionController::Base.allow_rescue = false
 begin
   DatabaseCleaner.strategy = :transaction
 rescue NameError
-  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+  raise 'You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it.'
 end
 
 Cucumber::Rails::Database.javascript_strategy = :truncation
