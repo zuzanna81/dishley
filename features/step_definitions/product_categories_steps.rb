d86443c4ea018680590755e4f797192a7e79cb9a
aljoh <reachingalfred@gmail.com>
2018-01-15 16:43:05 +0100

    product category feature tests in progress

--- features/step_definitions/product_categories_steps.rb
+++ features/step_definitions/product_categories_steps.rb
@@ -1,6 +1,11 @@
 Given("the following products exist within a specific restaurant and category") do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
+  table.hashes.each do |product|
+    restaurant = Restaurant.find_by(name: product[:restaurant])
+    category = FactoryBot.create(:product_category, name: product[:category])
+    FactoryBot.create(:product, name: product[:name],
+                                          product_category: category,
+                                          restaurant: restaurant)
+  end
 end
 
 Then("I would like to see {string} under the {string} category") do |string, string2|
