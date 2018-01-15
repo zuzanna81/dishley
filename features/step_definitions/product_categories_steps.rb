2287e15b33266b4db7acac5af310bbafb6b5c2e8
aljoh <reachingalfred@gmail.com>
2018-01-15 16:35:31 +0100

    sets up acceptance tests for product categories

---
+++ features/step_definitions/product_categories_steps.rb
@@ -0,0 +1,8 @@
+Given("the following products exist within a specific restaurant and category") do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I would like to see {string} under the {string} category") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
