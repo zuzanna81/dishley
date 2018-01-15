9f847881d23e6ba4fd84ec98b39de65757002f93
aljoh <reachingalfred@gmail.com>
2018-01-15 20:56:26 +0100

    adds association for restaurant and product_category

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,7 @@ class Restaurant < ApplicationRecord
   geocoded_by :full_address
   after_validation :geocode
   has_many :products
+  has_many :product_categories
   belongs_to :restaurant_category
 
   def full_address
