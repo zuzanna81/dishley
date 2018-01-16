d7177eb65bb307a0cc7c1bc440dd8316ad7af0bb
Raoul DIFFOUO <diraulo@gmail.com>
2018-01-16 12:18:03 +0200

    Minor changes to try and kick build with SemaphoreCI

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,6 @@
+require 'coveralls'
+Coveralls.wear_merged!('rails')
+
 require 'cucumber/rails'
 
 Chromedriver.set_version '2.33'
@@ -21,7 +24,8 @@ ActionController::Base.allow_rescue = false
 begin
   DatabaseCleaner.strategy = :transaction
 rescue NameError
-  raise 'You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it.'
+  raise 'You need to add database_cleaner to your Gemfile (in the :test group)'\
+        ' if you wish to use it.'
 end
 
 Cucumber::Rails::Database.javascript_strategy = :truncation
