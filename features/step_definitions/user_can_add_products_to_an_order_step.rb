a157f380855fd8078fafe60c6437e7586da2456a
oliver_ochman <oliver.ochman@hotmail.com>
2018-01-18 18:16:33 +0100

    refactor step def

--- features/step_definitions/user_can_add_products_to_an_order_step.rb
+++ features/step_definitions/user_can_add_products_to_an_order_step.rb
@@ -19,8 +19,9 @@ Then("an order should have been created in the database") do
   expect(@order).not_to be nil
 end
 
-Then("{string} should be an order item") do |string|
+Then("{string} should be an order item") do |product_name|
+  #binding.pry
   @order = @order || Order.last
-  items_collection = @order.order_items.map{ |product| product.item.name }
+  items_collection = @order.order_items.map{ |product| @product.item.name }
   expect(items_collection).to include product_name
 end
