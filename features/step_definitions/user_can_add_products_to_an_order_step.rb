2e8f3ae04df9cedc7a02116438461c4273833fef
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2018-01-18 13:28:28 +0100

    Implement step def and add product connection to Add to order button

--- features/step_definitions/user_can_add_products_to_an_order_step.rb
+++ features/step_definitions/user_can_add_products_to_an_order_step.rb
@@ -1,13 +1,12 @@
 And("I click on {string} for {string}") do |button, product_name|
-  binding.pry
   product = Product.find_by(name: product_name)
   within "#product-#{product.id}" do
     click_link_or_button button
   end
 end
 
-Then("I should see {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Then("I should see {string}") do |content|
+  expect(page).to have_content content
 end
 
 Then("I should be on the restaurant {string} page") do |string|
