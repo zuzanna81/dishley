3eb2adc5f3311047814ca642df521fe49d95a5c7
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2018-01-18 11:48:32 +0100

    Add unit tests for order and order items

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to respond_to :order_items }
+  it { is_expected.to respond_to :add }
 end
