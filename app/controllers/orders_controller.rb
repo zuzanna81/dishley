ba846197e83e950eb53ed17ef1557ac541c513db
oliver_ochman <oliver.ochman@hotmail.com>
2018-01-18 16:24:50 +0100

    add flash confirmation for adding product to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,20 +2,19 @@ class OrdersController < ApplicationController
   # before_action :get_product, only: [:create, :update]
 
   def create
-    binding.pry
+    #binding.pry
     order = Order.create
-    product = Product.find(params[:product_id])
-    order.add(product, product.price)
+    @product = Product.find(params[:product_id])
+    order.add(@product, @product.price)
     if order.persisted?
       session[:order_id] = order.id
+      redirect_back_with_notice
     end
   end
 
 
   private
-  #
-  # def get_product
-  #   binding.pry
-  #   @product = Product.find(:id)
-  # end
+  def redirect_back_with_notice
+    redirect_back(fallback_location: root_path, notice: "#{@product.name} has been added to your order")
+  end
 end
