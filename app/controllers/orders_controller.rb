6e24d417c4b02335671c6619309360ad3753c801
oliver_ochman <oliver.ochman@hotmail.com>
2018-01-18 22:00:37 +0100

    WiP price fixes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   # before_action :get_product, only: [:create, :update]
 
   def create
+    binding.pry
     order = Order.create
     @product = Product.find(params[:product_id])
     order.add(@product, @product.price)
