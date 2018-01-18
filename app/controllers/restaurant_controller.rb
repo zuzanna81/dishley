90b9ad269403e34ec6495f4417673ffbfc92e386
Francesco <francesco.falchi@gmail.com>
2018-01-18 10:56:46 +0100

    Add show near restaurants in restaurants controller and data geocoded to layout

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -1,10 +1,25 @@
 class RestaurantController < ApplicationController
+  before_action :coordinates, only: :index
+
   def index
+    @restaurants = Restaurant.near([@coordinates[:latitude], @coordinates[:longitude]], 50)
     @restaurants = Restaurant.all
     @restaurant_categories = RestaurantCategory.all
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+  def coordinates
+    if params[:lat].present? && params[:lng].present?
+      @coordinates = {latitude: params[:lat],
+                      longitude: params[:lng]}
+      @geocoded = true
+    else
+      @coordinates = request.location.data.symbolize_keys
+      # @geocoded = true
+    end
   end
 end
