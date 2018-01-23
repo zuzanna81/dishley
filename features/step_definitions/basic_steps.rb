680bea00a79bc72023af8f4265098b652d34fb51
Francesco <francesco.falchi@gmail.com>
2018-01-23 09:19:03 +0100

    Delete landing steps file and add steps to basic steps file

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -10,6 +10,18 @@ Then("I should be redirected to the landing page") do
   expect(current_path).to eq root_path
 end
 
+Then("I would like to see {string}") do |content|
+  expect(page).to have_content content
+end
+
 Given("pause") do
   binding.pry
+end
+
+When("I visit the landing page") do
+  visit root_path
+end
+
+When("show me the page") do
+  save_and_open_page
 end
