06d7617f2952a7ae5309ce417c52d08a5d0ccbe5
Francesco <francesco.falchi@gmail.com>
2018-01-23 17:48:49 +0100

    Delete payment confirmation file and add steps to payment steps

--- features/step_definitions/payment_confirmation_steps.rb
+++
@@ -1,9 +0,0 @@
-Then("the order should be {string}") do |state|
-  sleep 2
-  order = Order.last
-  expect(order.state).to eq state
-end
-
-Then("I should see {string} and the total amount should be {string}") do |text, price|
-  expect(page).to have_content "#{text} #{price}"
-end
--- features/step_definitions/payment_steps.rb
+++ features/step_definitions/payment_steps.rb
@@ -2,7 +2,7 @@ Given('I visit orders page') do
   expect(current_path).to eq orders_path
 end
 
-When("I click on {string} button") do |element|
+When('I click on {string} button') do |element|
   sleep 5
   click_button element
   sleep 2
@@ -27,4 +27,14 @@ end
 
 Then('I should not be redirected to order page') do
   expect(page.current_path).not_to eq orders_path
+end
+
+Then('the order should be {string}') do |state|
+  sleep 2
+  order = Order.last
+  expect(order.state).to eq state
+end
+
+Then('I should see {string} and the total amount should be {string}') do |text, price|
+  expect(page).to have_content "#{text} #{price}"
 end
