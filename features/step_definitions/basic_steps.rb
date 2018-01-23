590f336397acd4fd1bf4ebe72da584d16e39d578
Francesco <francesco.falchi@gmail.com>
2018-01-23 09:50:02 +0100

    Beautify basic steps

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,27 +1,27 @@
-Then("the {string} should be visible") do |selector|
+Then('the {string} should be visible') do |selector|
   expect(page).to have_selector :css, "##{selector}"
 end
 
-When("I click on {string} link") do |link|
+When('I click on {string} link') do |link|
   click_link link
 end
 
-Then("I should be redirected to the landing page") do
+Then('I should be redirected to the landing page') do
   expect(current_path).to eq root_path
 end
 
-Then("I would like to see {string}") do |content|
+Then('I would like to see {string}') do |content|
   expect(page).to have_content content
 end
 
-Given("pause") do
+Given('pause') do
   binding.pry
 end
 
-When("I visit the landing page") do
+When('I visit the landing page') do
   visit root_path
 end
 
-When("show me the page") do
+When('show me the page') do
   save_and_open_page
 end
--- features/step_definitions/product_steps.rb
+++ features/step_definitions/product_steps.rb
@@ -55,6 +55,16 @@ Then('I would not like to see {string} under the {string} category') do |product
   end
 end
 
+Then('I would like to see an image for {string}') do |product_name|
+  product = Product.find_by(name: product_name)
+  expect(page).to have_css("img[src='#{product.image_file_link}']")
+end
+
+Then('I would not like to see an image for {string}') do |product_name|
+  product = Product.find_by(name: product_name)
+  expect(page).not_to have_css("img[src='#{product.image_file_link}']")
+end
+
 When('I visit the {string} show page') do |restaurant_name|
   restaurant = Restaurant.find_by(name: restaurant_name)
   visit restaurant_path(restaurant)
