9c980535529b3466d487a95a996fca4233d41831
Francesco <francesco.falchi@gmail.com>
2018-01-24 17:16:36 +0100

    Create api controller, make restaurant controller inherit from api controller, remove redundant data from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules.
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
