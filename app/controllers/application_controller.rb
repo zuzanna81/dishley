eb53b9629bbe033f07aa00a73bf2c53be6886f3f
Francesco <francesco.falchi@gmail.com>
2018-01-24 16:49:25 +0100

    Generate user auth with devise_token_auth:install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
 end
